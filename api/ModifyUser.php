fd94301167e072b620c9b28a8cf0774e39d78559
AKGM-17 <ekaintorresmiranda@gmail.com>
2026-01-22 10:30:48 +0100

    validaciones

--- api/ModifyUser.php
+++ api/ModifyUser.php
@@ -24,25 +24,24 @@ $controller = new controller();
 $modify = $controller->modifyUser($email, $username, $telephone, $name, $surname, $gender, $card_no, $profile_code);
 
 //if (!filter_var($telephone, FILTER_VALIDATE_INT) == false) {
+try{
     if ($modify) {
         header("Content-Type: application/json; charset=UTF-8"); 
         http_response_code(200);
-        echo json_encode([
-            "success"   => true,
-            "code" => 200,
-            "message" => "User modified correctly.",
-            "data" => "works"
-        ]);
+        echo json_encode([ "success"   => true, "code" => 200, "message" => "User modified correctly.", "data" => "works" ]);
     } else {
-        header("Content-Type: application/json; charset=UTF-8"); 
-        http_response_code(500);
-        echo json_encode([
-            "success"   => false,
-            "code" => 500,
-            "message" => "Error modifying the user.",
-            "data" => ""
-        ]);
+        
+            header("Content-Type: application/json; charset=UTF-8"); 
+            http_response_code(400);
+            echo json_encode([ "success"   => false, "code" => 400, "message" => "Error Updating data", "data" => "" ]);
     }
+}catch(Exception $e){
+     header("Content-Type: application/json; charset=UTF-8"); 
+        http_response_code(500);
+        echo json_encode([ "success"   => false, "code" => 500, "message" => "Server error", "data" => "" ]);
+        
+}
+    
 /*}else{
     header("Content-Type: application/json; charset=UTF-8"); 
     http_response_code(400);
@@ -51,6 +50,6 @@ $modify = $controller->modifyUser($email, $username, $telephone, $name, $surname
         "code" => 400,
         "message" => "Error: incorrect data inputed.",
         "data" => ""
-    ]);
-}*/
+    ]);*/
+
 ?>
